aef740907301646e7d9e26465654bac74ec97f6a
Devin Vander Stelt <devin@vstelt.com>
2022-07-14 10:32:39 -0400

    Add read and write commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{env::args, time::Duration};
+use std::{env::args, time::Duration, io::{stdin, Read}};
 
-use btleplug::{platform::Manager, api::{Manager as _, ScanFilter, Central, Peripheral}};
+use btleplug::{platform::Manager, api::{Manager as _, ScanFilter, Central, Peripheral, Characteristic, WriteType}};
 use tokio::time;
 use std::error::Error;
 
@@ -39,8 +39,39 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
             ping(&args[2]).await?;
         }
-        "read" => {},
-        "write" => {},
+        "read" => {
+            if args.get(2).is_none() {
+                eprintln!("No address specified\n");
+                help();
+                return Ok(());
+            }
+
+            if args.get(3).is_none() {
+                eprintln!("No characteristic uuid specified\n");
+                help();
+                return Ok(());
+            }
+
+            read(&args[2], &args[3]).await?;
+        },
+        "write" => {
+            if args.get(2).is_none() {
+                eprintln!("No address specified\n");
+                help();
+                return Ok(());
+            }
+
+            if args.get(3).is_none() {
+                eprintln!("No characteristic uuid specified\n");
+                help();
+                return Ok(());
+            }
+
+            let mut input = String::new();
+            stdin().read_to_string(&mut input).unwrap();
+
+            write(&args[2], &args[3], &input).await?;
+        },
         "help" => help(),
         _ => {
             eprintln!("Unrecognized command '{}'\n", args[1]);
@@ -95,10 +126,84 @@ async fn ping(addr: &str) -> Result<(), Box<dyn Error>> {
     // Print out the device servers and characteristics
     println!("Services:");
     for s in dev.services() {
-        println!("{s:?}");
+        println!("{}:", s.uuid);
+
+        for c in s.characteristics.iter() {
+            println!("\t{}: {:?}", c.uuid, c.properties);
+        }
     }
 
-    println!("Characteristics:");
+    Ok(())
+}
+
+async fn read(addr: &str, char_id: &str) -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await?;
+    let adapters = manager.adapters().await?;
+    let central = adapters.into_iter().nth(0).unwrap();
+
+    central.start_scan(ScanFilter::default()).await?;
+    time::sleep(Duration::from_secs(3)).await;
+
+    let mut dev = None;
+    for p in central.peripherals().await? {
+        if p.properties().await?.unwrap().address.to_string().eq(addr) {
+            dev = Some(p);
+        }
+    }
+
+    if dev.is_none() {
+        eprintln!("Unable to find device");
+        return Ok(());
+    }
+
+    let dev = dev.unwrap();
+    dev.connect().await?;
+
+    println!("Connected");
+    dev.discover_services().await?;
+
+    let chars = dev.characteristics();
+    let ch = chars.iter()
+        .find(|a| a.uuid.to_string().eq(char_id))
+        .unwrap();
+
+    let res = dev.read(ch).await?;
+    println!("{:?}", res);
+    Ok(())
+}
+
+async fn write(addr: &str, char_id: &str, msg: &str) -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await?;
+    let adapters = manager.adapters().await?;
+    let central = adapters.into_iter().nth(0).unwrap();
+
+    central.start_scan(ScanFilter::default()).await?;
+    time::sleep(Duration::from_secs(3)).await;
+
+    let mut dev = None;
+    for p in central.peripherals().await? {
+        if p.properties().await?.unwrap().address.to_string().eq(addr) {
+            dev = Some(p);
+        }
+    }
+
+    if dev.is_none() {
+        eprintln!("Unable to find device");
+        return Ok(());
+    }
+
+    let dev = dev.unwrap();
+    dev.connect().await?;
+
+    println!("Connected");
+    dev.discover_services().await?;
+
+    let chars = dev.characteristics();
+    let ch = chars.iter()
+        .find(|a| a.uuid.to_string().eq(char_id))
+        .unwrap();
+
+    dev.write(ch, msg.as_bytes(), WriteType::WithoutResponse).await?;
 
     Ok(())
 }
